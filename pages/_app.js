4c08cd57bcbd890b432bd1035e1c49258d0bc6c2
Beatriz Sousa <beasousa20@outlook.com>
2021-01-26 13:17:56 -0300

    Primeiro commit do projeto

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,36 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+   * {
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+    }
+
+    html, body {
+      height: 100% !important;
+    }
+
+    body {
+      display: flex;
+      flex-direction: column;
+      font-family: 'Lato', sans-serif;
+      color: ${({ theme }) => theme.colors.contrastText};
+      -webkit-font-smoothing: antialiased !important;
+    }
 `
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+const theme = db.theme;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
+
+        <GlobalStyle />
       </ThemeProvider>
     </>
   )
